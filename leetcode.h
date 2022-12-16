a0f0b399cb52b4c309a31f5f1593298e59ea59ab
Kargath <40135358+KargathEx@users.noreply.github.com>
2022-12-16 12:16:00 +0800

    Add files via upload

---
+++ leetcode.h
@@ -0,0 +1,178 @@
+#ifndef LC
+#define LC
+#include "pprint.h"
+#include<iostream>
+#include<regex>
+#include<sstream>
+#include<string>
+#include<type_traits>
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+namespace lc
+{
+  struct ListNode
+  {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next_) : val(x), next{ next_ } {}
+  };
+  class List
+  {
+    ListNode* head;
+    ListNode* copy(ListNode* p)
+    {
+      if (!p)return nullptr;
+      ListNode* r = new ListNode(p->val);
+      r->next = copy(p->next);
+      return r;
+    }
+  public:
+    List(const std::string& inputStr);
+    List(ListNode* p) { head = copy(p); };
+    ~List();
+    void print();
+    auto get() { return head; }
+  };
+
+  const int32_t TREE_NODE_BOUNDNARY = INT_MIN + 1; // replace null
+  struct TreeNode
+  {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x = 0) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+    ~TreeNode() {
+      if (left) delete left;
+      if (right) delete right;
+    }
+  };
+
+  //template for int and long long
+
+//for int and long long
+  template<typename T>
+  std::vector<T> Vec(const std::string inputStr)
+  {
+    std::vector<T> ret;
+    const std::string pattern = R"(([+|-]?\w*\.?[\w+|#|(|)|\.]),?)";
+    std::smatch match;
+    std::string log = inputStr;
+    while (regex_search(log, match, (std::regex)pattern)) {
+      std::stringstream ss;
+      std::string treeNull = match.str(1) == "null" ? std::to_string(TREE_NODE_BOUNDNARY) : match.str(1);
+      ss << treeNull;
+      T temp;
+      ss >> temp;
+      ret.push_back(temp);
+      log = match.suffix();
+    }
+    if (ret.empty())
+      std::cout << "Construct Vector failed" << std::endl;
+    return ret;
+  }
+  template<typename T>
+  std::vector<std::vector<int>> Vec2(const std::string inputStr)
+  {
+    std::vector<std::vector<T>> ret;
+    const std::string pattern = R"((\[[^\[\]]*\]))";
+    std::smatch match;
+    std::string log = inputStr;
+    while (regex_search(log, match, (std::regex)pattern)) {
+      const std::string vec = match.str(1);
+      ret.push_back(Vec<int>(vec));
+      log = match.suffix();
+    }
+    if (ret.empty()) {
+      ret = {};
+      std::cout << "Construct vector<vector<int>>failed" << std::endl;
+    }
+    return ret;
+  }
+
+  //string
+    //from:https://leetcode.cn/playground/new/empty/
+
+  inline vector<string> VecStr(const string input) {
+    if (input.size() < 2) {
+      std::cout << "Construct vector<string> failed" << std::endl;
+      return{};
+    }
+    vector<string> res;
+    string result;
+    for (int i = 1; i < input.length() - 1; i++) {
+      char currentChar = input[i];
+      if (input[i] == '\\') {
+        char nextChar = input[i + 1];
+        switch (nextChar) {
+        case '\"': result.push_back('\"'); break;
+        case '/': result.push_back('/'); break;
+        case '\\': result.push_back('\\'); break;
+        case 'b': result.push_back('\b'); break;
+        case 'f': result.push_back('\f'); break;
+        case 'r': result.push_back('\r'); break;
+        case 'n': result.push_back('\n'); break;
+        case 't': result.push_back('\t'); break;
+        default: break;
+        }
+        i++;
+      }
+      else {
+        result.push_back(currentChar);
+      }
+      if (input[i] == ',')
+      {
+        result.pop_back();
+        result.pop_back();
+        res.push_back(result.substr(1));
+        result = "";
+      }
+    }
+    if (!result.empty()) {
+      result.pop_back();
+      res.push_back(result.substr(1));
+    }
+    return res;
+  }
+  inline std::vector<std::vector<string>> Vec2Str(const std::string inputStr)
+  {
+    std::vector<std::vector<string>> ret;
+    const std::string pattern = R"((\[[^\[\]]*\]))";
+    std::smatch match;
+    std::string log = inputStr;
+    while (regex_search(log, match, (std::regex)pattern)) {
+      const std::string vec = match.str(1);
+      ret.push_back(VecStr(vec));
+      log = match.suffix();
+    }
+    if (ret.empty()) std::cout << "Construct vector<vector<string>> failed" << std::endl;
+    return ret;
+  }
+
+  class Tree
+  {
+  private:
+    TreeNode* root;
+    void print_Impl(TreeNode* n, bool left, std::string const& indent);
+    TreeNode* copyTree(TreeNode* p)
+    {
+      if (!p)return nullptr;
+      auto r = new TreeNode(p->val);
+      r->left = copyTree(p->left);
+      r->right = copyTree(p->right);
+      return r;
+    }
+  public:
+    Tree(const std::string& input);
+    Tree(TreeNode* p) { root = copyTree(p); }
+    ~Tree();
+    TreeNode* get() { return root; }
+    void print();
+  };
+  void print(TreeNode* p);
+  void print(ListNode* p);
+}
+#endif
---
+++ main.cpp
@@ -0,0 +1,51 @@
+﻿// leetcode-parser.cpp : Defines the entry point for the application.
+//
+
+#include<vector>
+#include<string>
+#include<map>
+#include<algorithm>	
+#include <unordered_map>
+#include<iostream>
+#include<bitset>
+#include<algorithm>
+#include<fstream>
+#include<numeric>
+#include<set>
+#include<queue>
+#include<functional>
+#include<iomanip>
+#include<variant>
+#include<cassert>
+#include<memory>
+#include<iostream>
+#include<cctype>
+#include "pprint.h"
+#include "Solution.h"
+#include"leetcode.h"
+using namespace std;
+using namespace lc;
+auto stc(vector<string>& t1)
+{
+  vector<vector<char>> t;
+  for (auto i : t1)
+  {
+    t.push_back(vector<char>());
+    for (auto j : i)
+      t.back().push_back(j);
+  }
+  return t;
+}
+
+
+int main()
+{
+  std::ifstream file("in.txt");
+  std::string in;
+  std::getline(file, in);
+  auto res = lc::VecStr(in);  //还是不行.
+  for (auto i : res)cout << i << " ";
+  return 0;
+}
+//你可能会发现它很有用
+
